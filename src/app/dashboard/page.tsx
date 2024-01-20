7c441b5b268588483762b239c895c44f5a9b572d
ShankarAswal <shankaraswal@gmail.com>
2024-01-20 05:40:12 +0000

    added parallel routing or SLOT functionality (#4)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <div className="p-5">--: this data comming from page.tsx</div>;
 };
 
 export default Dashboard;
