76846b7d0fd71cf381fd40cfeb3d4a91c71450cd
ShankarAswal <shankaraswal@gmail.com>
2024-01-22 03:36:19 +0000

    added: intercepting routes (#7)

---
+++ src/app/intercepting/comp1/(.)comp2/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+export default () => {
+  return (
+    <>
+      <h2 className="mb-4 text-red-800">
+        (.)--: This is Intercepting Component of Comp 2
+      </h2>
+    </>
+  );
+};
---
+++ src/app/intercepting/comp1/comp2/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import React from "react";
+
+export default () => {
+  return (
+    <>
+      <h2 className="mb-4">This is Component 2</h2>
+      <Link href="/intercepting/comp1">Go back to Comp 1</Link>
+    </>
+  );
+};
