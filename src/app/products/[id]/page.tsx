4794bd8dd1ce580091a1047cd4d2070d53a0bdf7
Shankar Aswal <shankaraswal@gmail.com>
2024-01-22 20:37:46 +0000

    fixed: linting error

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -41,7 +41,7 @@ const ProductDetail = ({ params }: Props) => {
         </div>
         <p className="text-justify">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
@@ -53,7 +53,7 @@ const ProductDetail = ({ params }: Props) => {
         <br />
         <p className="text-justify">
           Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          industry. Lorem Ipsum has been the industrys standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
           scrambled it to make a type specimen book. It has survived not only
           five centuries, but also the leap into electronic typesetting,
