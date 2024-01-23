770497229c613174d24f3a2ed289ce4dc69df163
ShankarAswal <shankaraswal@gmail.com>
2024-01-23 19:09:58 +0000

    added route links on home page (#11)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,54 @@
 import Link from "next/link";
-import RouteHandlers from "./route-handlers/page";
 
 export default function Home() {
   return (
     <div className="tetx-left">
       <h1 className="mb-5">Home page</h1>
+    <nav className="bg-red-200">
+      <Link
+        href="/blog"
+        className="text-sm text-teal-100 rounded m-3 p-3 font-bold underline block"
+      >
+        Blog Example
+      </Link>
+      <Link
+        href="/dashboard"
+        className="text-sm text-black text-left border border-red-500 m-3 p-3 font-bold underline block"
+      >
+        Dashboard Example
+      </Link>
+      <Link
+        href="/docs"
+        className="text-sm text-black text-left border border-red-500 m-3 p-3 font-bold underline block"
+      >
+        Docs Example
+      </Link>
+      <Link
+        href="/products"
+        className="text-sm text-black text-left border border-red-500 m-3 p-3 font-bold underline block"
+      >
+        Products List Example
+      </Link>
 
       <Link
-        href="/route-handlers"
+        href="/intercepting/comp1"
         className="text-sm text-black text-left border border-red-500 m-3 p-3 font-bold underline block"
       >
-        RouteHandlers Example
+        Route Intercepting Example
       </Link>
       <Link
         href="/route-handlers"
         className="text-sm text-black text-left border border-red-500 m-3 p-3 font-bold underline block"
       >
-        RouteHandlers Example
+        Route Handlers Example
       </Link>
       <Link
-        href="/route-handlers"
+        href="/user"
         className="text-sm text-black text-left border border-red-500 m-3 p-3 font-bold underline block"
       >
-        RouteHandlers Example
+        User Profile Pages
       </Link>
-      <Link
-        href="/route-handlers"
-        className="text-sm text-black text-left border border-red-500 m-3 p-3 font-bold underline block"
-      >
-        RouteHandlers Example
-      </Link>
+      </nav>
     </div>
   );
 }
